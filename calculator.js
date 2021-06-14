2e1d257b5448f1fda952889fe9a41c93eb7349b6
Erdem <balli@campus.tu-berlin.de>
2021-06-14 20:58:52 +0200

    added multiplication with a wrong test

--- calculator.js
+++ calculator.js
@@ -7,8 +7,13 @@ function sub(a, b) {
     return a - b;
 }
 
+function mul(a, b){
+    return a * b;
+}
+
 //node exports the functions so we can use them in the test
 module.exports = {
     add: add,
     sub: sub,
+    mul: mul,
 }
--- calculator.test.js
+++ calculator.test.js
@@ -9,3 +9,7 @@ test('adds 4 + 5 to equal 9', () => {
   test('sub 10 - 5 to equal 5', () => {
     expect(sub(10, 5)).toBe(5);
   });
+
+  test('mul 10 * 5 to equal 78', () => {
+    expect(mul(10, 5)).toBe(78);
+  });
