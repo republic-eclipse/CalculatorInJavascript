8bf28a28edacec5a5e8b5e58f375c36c850eeccc
Erdem <balli@campus.tu-berlin.de>
2021-06-21 12:01:15 +0200

    reseting

--- calculator.js
+++ calculator.js
@@ -7,9 +7,6 @@ function sub(a, b) {
     return a - b;
 }
 
-function mul(a, b){
-    return a * b;
-}
 
 //node exports the functions so we can use them in the test
 module.exports = {
--- calculator.test.js
+++ calculator.test.js
@@ -9,7 +9,3 @@ test('adds 4 + 5 to equal 9', () => {
   test('sub 10 - 5 to equal 5', () => {
     expect(sub(10, 5)).toBe(5);
   });
-
-  test('mul 10 * 5 to equal 78', () => {
-    expect(mul(10, 5)).toBe(78);
-  });
